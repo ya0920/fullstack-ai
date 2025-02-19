7273abcf4145ddf8622e21ca26123b72124b90c1
ya0920 <1412361132@qq.com>
2025-02-19 23:35:51 +0800

    跨域

---
+++ 面试题/字节/跨域/websocket/server/index.js
@@ -0,0 +1,20 @@
+const WebSocket = require('ws');
+
+// 在 3000 端口上建立 WebSocket 伺服器 （指随时都在线的服务）
+const ws = new WebSocket.Server({ port: 3000 });
+
+let count = 0
+
+ws.on('connection', (obj) => {
+    console.log(obj);
+    obj.on('message', (msg) => {  // 收到客户端发来的消息
+        // console.log(msg.toString());
+        obj.send('收到了')
+
+        setInterval(() => {
+            count++
+            obj.send(count)
+        }, 2000)
+
+    })
+})
