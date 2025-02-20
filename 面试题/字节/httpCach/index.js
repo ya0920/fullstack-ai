3525f6a58e1efa52e54056814a75e8920c77d8c8
ya0920 <1412361132@qq.com>
2025-02-20 17:52:58 +0800

    浏览器缓存

---
+++ 面试题/字节/httpCach/index.js
@@ -0,0 +1,49 @@
+const http = require('http');
+const url = require('url');
+
+const responseData = {
+    id: 1,
+    name: '张三',
+    age: 18,
+    sex: '男'
+}
+
+function toHTML(data) {
+    return `
+    <ul>
+      <li><span>id:</span> <span>${data.id}</span></li>
+      <li><span>昵称:</span> <span>${data.name}</span></li>
+      <li><span>年龄:</span> <span>${data.age}</span></li>
+      <li><span>性别:</span> <span>${data.sex}</span></li>
+    </ul>
+  `
+}
+
+
+const server = http.createServer((req, res) => {
+    const { pathname } = url.parse(req.url)
+    if (pathname === '/') {
+        const accept = req.headers.accept // 前端想要的类型
+        // console.log(accept);
+
+        if (accept.includes('application/json')) {
+            res.writeHead(200, { 'content-type': 'application/json' })
+            res.end(JSON.stringify(responseData))
+        } else {
+            res.writeHead(200, { 'content-type': 'text/html; charset=utf-8' })
+            res.end(toHTML(responseData))
+        }
+
+        // res.writeHead(200, {'content-type': 'text/html'})  // 告诉浏览器返回的内容的类型
+        // res.end('<h1>Hello World</h1>')
+
+    } else {
+        res.writeHead(404, { 'content-type': 'text/html' })
+        res.end('<h1>Not Found</h1>')
+    }
+
+})
+
+server.listen(3000, () => {
+    console.log('server is running on port 3000');
+})
---
+++ 面试题/字节/归并排序/index.js
@@ -0,0 +1,60 @@
+let arr = [7, 8, 4, 5, 3, 9, 1, 6]
+
+// [7, 8, 4, 5]  [3, 9, 1, 6]
+// [7, 8] [4, 5]      [3, 9] [1, 6]
+// [7] [8] [4] [5]    [3] [9] [1] [6]
+
+// [7, 8] [4, 5]      [3, 9] [1, 6]
+// [4, 5, 7, 8]      [1, 3, 6, 9]
+// [1, 3, 4, 5, 6, 7, 8, 9]
+
+
+function mergeSort(arr) {
+  const len = arr.length
+  if (len <= 1) {
+    return arr
+  }
+  // 找分割点
+  const mid = Math.floor(len / 2)
+
+  const leftArr = mergeSort(arr.slice(0, mid))
+
+  const rightArr = mergeSort(arr.slice(mid, len))
+
+  arr = mergeArr(leftArr, rightArr) // 合并两个有序数组，依旧是有序的
+  return arr
+}
+
+
+function mergeArr(arr1, arr2) {
+  let i = 0, j = 0
+  let res = []
+  const len1 = arr1.length
+  const len2 = arr2.length
+  while (i < len1 && j < len2) {
+    if (arr1[i] < arr2[j]) {
+      res.push(arr1[i])
+      i++
+    } else {
+      res.push(arr2[j])
+      j++
+    }
+  }
+
+  if (i < len1) {
+    res = res.concat(arr1.slice(i)) // 将剩余的部分连接到res后面
+  } else {
+    res = res.concat(arr2.slice(j))
+  }
+
+  return res
+
+}
+
+
+
+console.log(mergeSort(arr));
+
+
+// [1, 2, 3, 5, 7]
+// [4, 6, 8, 9]
