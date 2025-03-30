b6570517727b1fbf11f631298a3ef17d92d13892
ya0920 <1412361132@qq.com>
2025-03-31 00:06:37 +0800

    2

--- vue/blog/blog-client/src/utils/isLogin.js
+++
@@ -1,7 +0,0 @@
-export function isLogin() {
-  if (localStorage.getItem('token') || sessionStorage.getItem('token')) {
-    return true;
-  }else {
-    return false;
-  }
-}
--- vue/blog/blog-client/src/utils/randomColor.js
+++
@@ -1,6 +0,0 @@
-export function randomColor(min, max) {
-    let r = Math.floor((Math.random() * (max - min) + min));
-    let g = Math.floor((Math.random() * (max - min) + min));
-    let b = Math.floor((Math.random() * (max - min) + min));
-    return `rgb(${r},${g},${b})`;
-}
